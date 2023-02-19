035beca01d9ef3a02efcb8e47a2611fdf17a5926
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-02-19 23:50:01 +0900

    Chap 19

---
+++ src/chap19/chap19excercise.java
@@ -0,0 +1,10 @@
+package chap19;
+
+public class chap19excercise {
+}
+
+/*1 2
+2 24
+3. new Socket("localhost", 5001); serverSocket.accept()
+4 InputStream / OutputStream OutputStream / InputStream
+5 ① Datagram Socket ② DatagramPacket ③ Datagram Socket ④ DatagramPacket⑤ DatagramPacket */
